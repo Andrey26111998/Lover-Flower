7029c607dfb6dd3ea08e5f4116aee0279a6ca83e
Andrii <skrillex26111998@gmail.com>
2025-08-19 11:17:27 +0300

    404, About, DeliveryPayment, FAQ, Home, Order, PaymentError, SuccessfulPayment pages optimized page structure

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import {defineConfig} from "vite";
+import viteHTMLIncludes from "@kingkongdevs/vite-plugin-html-includes";
 
 export default defineConfig({
+    plugins: [
+        viteHTMLIncludes({
+            componentsDir: "/components/",
+        })
+    ],
     build: {
         rollupOptions: {
             input: {
                 main: "index.html",
                 order: "order.html",
+                notFound: "404.html",
+                about: "about.html",
+                deliveryPayment: "delivery-payment.html",
+                faq: "faq.html",
+                paymentError: "payment_error.html",
+                successfulPayment: "successful_payment.html",
             },
         },
     },
