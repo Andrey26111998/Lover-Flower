9c5f1892f2f0d8ff00e93cd35ddaa6c8a7963cc5
Andrii <skrillex26111998@gmail.com>
2025-08-21 15:36:04 +0300

    ContactsPage contacts section created

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
                 faq: "faq.html",
                 paymentError: "payment_error.html",
                 successfulPayment: "successful_payment.html",
+                contacts: "contacts.html",
             },
         },
     },
